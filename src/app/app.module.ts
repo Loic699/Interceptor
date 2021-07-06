e0500ef242a3931946d4ba07108f532323c845d7
Loic699 <loicpimet@icloud.com>
2021-07-06 10:11:00 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { LogInterceptor } from './core/log.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: LogInterceptor,
+    multi: true
+}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
---
+++ src/app/core/log.interceptor.ts
@@ -0,0 +1,34 @@
+import { Injectable } from  '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from  '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export  class  LogInterceptor implements HttpInterceptor{
+    intercept(req:  HttpRequest<any>, next:  HttpHandler):  Observable<HttpEvent<any>> {
+        console.log(req.urlWithParams); // J'affiche l'url avec les params
+        // Je passe la requête à la suite des interceptors cachés d'angular
+        // Et je retourne ce résultat pour que la requête ait bien lieu
+        var start = new Date().getTime();
+        return  next.handle(req).pipe(
+            // tap est un observable qui indique: 
+            // Je fais des actions sans modifier la réponse
+            tap((response) => {
+                    // On s'assure qu'il s'agit bien d'une réponse http
+                    if (response  instanceof  HttpResponse) {
+                            // Ici sera le code exécuté à la réponse du serveur
+                            // Dans le cas où tout s'est bien passé
+                            var elapsed = new Date().getTime() - start;
+                            console.log(elapsed)
+                    }
+            })
+        );
+}
+
+
+}
+
+
+
+
+
